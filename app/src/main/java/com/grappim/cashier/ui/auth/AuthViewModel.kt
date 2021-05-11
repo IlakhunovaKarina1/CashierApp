e6934557d893cf0a86384ff3b845c795a31db60f
IlakhunovaKarina <grigoriymikhalchuk@hotmail.com>
2021-05-11 09:41:04 +0600

    Remove harcoded login

--- app/src/main/java/com/grappim/cashier/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/grappim/cashier/ui/auth/AuthViewModel.kt
@@ -26,10 +26,6 @@ class AuthViewModel @Inject constructor(
 
     init {
         prePopulateDb()
-        login(
-            "7023335353",
-            "qwe"
-        )
     }
 
     private val _password = MutableLiveData<String>()
